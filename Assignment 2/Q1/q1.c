61c3961640aafa8f0813c82eaebc0240454cefb7
vishweshvhavle <vishweshvhavle@gmail.com>
2021-10-06 01:03:15 +0530

    Fixed process order

--- Assignment 2/Q1/q1.c
+++ Assignment 2/Q1/q1.c
@@ -22,20 +22,18 @@ void SR(int ppid){
     char pid[10];
     sprintf(pid, "%d", ppid);
     char *args[]={"./E1", pid , NULL};
-    execv(args[0],args);
+    execv(args[0], args);
 }
 
 void ST(int ppid){
     char pid[10];
     sprintf(pid, "%d", ppid);
     char *args[]={"./E2", pid , NULL};
-    execv(args[0],args);
+    execv(args[0], args);
 }
-
 
 int main(){
     pid_t pid, pid_st, pid_sr;
-
     if ((pid = fork()) < 0){
         perror("\nError in fork()");
         exit(0);
@@ -43,13 +41,20 @@ int main(){
 
     if (pid == 0){
         signal(SIGTERM, sigterm);
+        struct itimerval val;
+        val.it_interval.tv_sec = 0;
+        val.it_interval.tv_usec = 0;
+        val.it_value.tv_sec = 2; 
+        val.it_value.tv_usec = 0;
+        setitimer(ITIMER_REAL, &val,0);
+        while (1);
+    }
 
+    else{
         if ((pid_st = fork()) < 0){
             perror("\nError in fork()");
             exit(0);
         }
-
-        int ppid = getppid();
 
         if (pid_st == 0){
             if ((pid_sr = fork()) < 0){
@@ -58,14 +63,12 @@ int main(){
             }
 
             if (pid_sr == 0){
-                SR(ppid);
+                SR(pid);
             }
 
-            ST(ppid);
-            sleep(2);
+            ST(pid);
         }
     }
-
     waitpid(pid, NULL, 0);
     return(0);
 }
